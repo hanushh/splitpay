fe35a570d71524d343be9dc9f2d790a51570f5b5
Hanush H Nair <hnair@adobe.com>
2026-03-19 19:57:35 +0530

    changes

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'jest-expo',
   setupFilesAfterEnv: [
     '@testing-library/react-native/build/matchers/extend-expect',
+    '<rootDir>/jest.setup.ts',
   ],
   transformIgnorePatterns: [
     '/node_modules/(?!(.pnpm|react-native|@react-native|@react-native-community|expo|@expo|@expo-google-fonts|react-navigation|@react-navigation|@sentry/react-native|native-base))',
